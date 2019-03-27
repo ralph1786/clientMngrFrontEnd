f0de576debc3e56e5f954059dbd306dcf82502b1
ralph1786 <ralph1786@gmail.com>
2019-03-27 17:32:51 -0400

    fixed styling removed comments

--- src/reducer/childReducer.js
+++ src/reducer/childReducer.js
@@ -14,22 +14,3 @@ const childReducer = (state = initialState, action) => {
 };
 
 export default childReducer;
-
-// const initialState = {
-//   children: [],
-//   searchTerm: "",
-//   filtered: []
-// };
-
-// const childReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "LOAD_CHILDREN":
-//       return { ...state, children: action.payload };
-//     case "CREATE_CHILD":
-//       return {...state};
-//     default:
-//       return state;
-//   }
-// };
-
-// export default childReducer;
