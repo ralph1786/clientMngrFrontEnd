a2a14c213121a542192e357a885968a381a21f53
ralph1786 <ralph1786@gmail.com>
2019-04-11 12:06:56 -0400

    added register_parent case to reducer

--- src/reducer/parentReducer.js
+++ src/reducer/parentReducer.js
@@ -4,6 +4,8 @@ const parentReducer = (state = initialState, action) => {
   switch (action.type) {
     case "LOAD_PARENTS":
       return action.payload;
+    case "REGISTER_PARENT":
+      return [...state, action.payload];
     default:
       return state;
   }
