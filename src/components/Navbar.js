29e9b7d9cca0e776fdcba2d9288bcb3b907c5c6b
ralph1786 <ralph1786@gmail.com>
2019-06-03 12:03:59 -0400

    added more buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,47 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 import LogOut from "./LogOut";
-import "./Navbar.css";
+import "./Navbar.scss";
 
 const Navbar = props => {
+  console.log(props);
   return (
     <div className="navbar">
       <Link to="/">
-        {/* <img
-          src="https://i.pinimg.com/originals/17/5d/67/175d6717fa33b624afc4664d76376370.jpg"
-          alt="logo"
-        /> */}
         <h2>daycareMngr</h2>
       </Link>
+
+      {props.location.pathname === "/edit" ||
+      props.location.pathname === "/create" ? (
+        <div
+          className={
+            props.location.pathname === "/edit"
+              ? "dashboard-button-edit-form"
+              : "dashboard-button"
+          }
+        >
+          <Link to="/dashboard">
+            <button>Dashboard</button>
+          </Link>
+        </div>
+      ) : null}
+
+      {props.location.pathname === "/edit_parent" ? (
+        <div className="parent-dashboard">
+          <Link to="/parent_dashboard">
+            <button>Dashboard</button>
+          </Link>
+        </div>
+      ) : null}
+
+      {props.location.pathname === "/dashboard" ||
+      props.location.pathname === "/edit" ? (
+        <div className="create-button">
+          <Link to="/create">
+            <button>New Child</button>
+          </Link>
+        </div>
+      ) : null}
       <span>
         {localStorage.getItem("token") &&
         localStorage.getItem("token") !== undefined ? (
