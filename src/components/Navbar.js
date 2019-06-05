592927e12ef862de54887f2acd12d1bf4d89cfd3
ralph1786 <ralph1786@gmail.com>
2019-06-05 12:54:21 -0400

    removed logic to separate component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
-import LogOut from "./LogOut";
 import "./Navbar.scss";
+import NavbarMenu from "./NavbarMenu";
 
 const Navbar = props => {
   console.log(props);
@@ -12,45 +12,7 @@ const Navbar = props => {
         <h2>daycareMngr</h2>
       </Link>
 
-      {props.location.pathname === "/edit" ||
-      props.location.pathname === "/create" ? (
-        <div
-          className={
-            props.location.pathname === "/edit"
-              ? "dashboard-button-edit-form"
-              : "dashboard-button"
-          }
-        >
-          <Link to="/dashboard">
-            <button>Dashboard</button>
-          </Link>
-        </div>
-      ) : null}
-
-      {props.location.pathname === "/edit_parent" ? (
-        <div className="parent-dashboard">
-          <Link to="/parent_dashboard">
-            <button>Dashboard</button>
-          </Link>
-        </div>
-      ) : null}
-
-      {props.location.pathname === "/dashboard" ||
-      props.location.pathname === "/edit" ? (
-        <div className="create-button">
-          <Link to="/create">
-            <button>New Child</button>
-          </Link>
-        </div>
-      ) : null}
-      <span>
-        {localStorage.getItem("token") &&
-        localStorage.getItem("token") !== undefined ? (
-          <LogOut />
-        ) : (
-          ""
-        )}
-      </span>
+      <NavbarMenu pathname={props.location.pathname} />
     </div>
   );
 };
