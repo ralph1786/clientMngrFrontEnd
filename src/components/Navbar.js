3c95ddf15b8a9917ec2a64ecf34bbb5029d23120
ralph1786 <ralph1786@gmail.com>
2019-06-28 14:38:42 -0400

    added search bar to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,28 @@ import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 import "./Navbar.scss";
 import NavbarMenu from "./NavbarMenu";
+import SearchBar from "./SearchBar";
+import { searchWord } from "../actions/childActions";
 
 const Navbar = props => {
+  const changeHandler = e => {
+    props.searchTerm(e.target.value);
+  };
+
   return (
     <div className={props.scrolled ? "navbar-scroll" : "navbar"}>
       <Link to="/">
         <h2>daycareMngr</h2>
       </Link>
+
+      {props.location.pathname === "/dashboard" && props.scrolled ? (
+        <div className="navbar-search-input">
+          <SearchBar
+            searchWord={props.searchWord}
+            onChange={e => changeHandler(e)}
+          />
+        </div>
+      ) : null}
 
       <NavbarMenu pathname={props.location.pathname} />
     </div>
@@ -18,8 +33,18 @@ const Navbar = props => {
 
 const mapStateToProps = state => {
   return {
-    provider: state.provider.provider
+    provider: state.provider.provider,
+    searchWord: state.children.searchWord
   };
 };
 
-export default withRouter(connect(mapStateToProps)(Navbar));
+const mapDispatchToProps = dispatch => ({
+  searchTerm: word => dispatch(searchWord(word))
+});
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Navbar)
+);
