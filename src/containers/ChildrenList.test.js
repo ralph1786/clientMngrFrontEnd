f2375d7ca410493795d81e2f7ad063f8fefdc00d
ralph1786 <ralph1786@gmail.com>
2019-09-09 10:08:48 -0400

    added snapshot, check for searchBar

---
+++ src/containers/ChildrenList.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { ChildrenList } from "./ChildrenList";
+import SearchBar from "../components/SearchBar";
+import { ChildCard } from "../components/ChildCard";
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(
+    <ChildrenList
+      children={[]}
+      searchTerm={""}
+      allChildren={() => {}}
+      search={() => {}}
+    />
+  );
+});
+
+it("captures a snapshot of the ChildrenList component", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("contains the SearchBar component", () => {
+  expect(wrapper.find(SearchBar)).toHaveLength(1);
+});
+
+it("expects to show a list of ChildCard components", () => {
+  expect(wrapper.find(ChildCard)).toBeTruthy();
+});
