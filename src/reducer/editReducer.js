250e6f22c160c53477a9032f9935100fa278f45e
ralph1786 <ralph1786@gmail.com>
2020-01-09 11:03:48 -0500

    changed init state

--- src/reducer/editReducer.js
+++ src/reducer/editReducer.js
@@ -1,15 +1,17 @@
-const initialState = {};
+const initialState = {
+  childToEdit: {}
+};
 
 const editReducer = (state = initialState, action) => {
   switch (action.type) {
     case "SELECTED_CHILD":
-      return action.payload;
-    case "EDIT_CHILD":
-      const editedChildren =
-        state.id === action.payload.id
-          ? { ...state, ...action.payload }
-          : state;
-      return editedChildren;
+      return { ...state, childToEdit: { ...action.payload } };
+    // case "EDIT_CHILD":
+    //   const editedChildren =
+    //     state.id === action.payload.id
+    //       ? { ...state, ...action.payload }
+    //       : state;
+    //   return editedChildren;
     default:
       return state;
   }
